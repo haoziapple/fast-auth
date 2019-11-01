342c67d6a89e36c968125a86ef8c795c94122e49
haoziapple <haozixiaowang@163.com>
2019-11-01 16:14:46 +0800

    完善uauth，添加p6spy用来监控SQL执行

--- uauth/src/main/java/github/haozi/uauth/config/DbConfig.java
+++ uauth/src/main/java/github/haozi/uauth/config/DbConfig.java
@@ -1,9 +1,11 @@
 package github.haozi.uauth.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.nutz.dao.Dao;
 import org.nutz.dao.impl.NutDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,7 +21,11 @@ public class DbConfig {
     private static final Logger log = LoggerFactory.getLogger(DbConfig.class);
 
     @Bean
-    public Dao nutDao(DataSource dataSource) {
+    public Dao nutDao(DataSource dataSource, @Value("${validationQuery}") String validationQuery) {
+        if(dataSource instanceof DruidDataSource) {
+            ((DruidDataSource) dataSource).setValidationQuery(validationQuery);
+        }
+
         Dao dao = new NutDao(dataSource);
         return dao;
     }
